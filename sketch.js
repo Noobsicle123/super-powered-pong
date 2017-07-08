464a6436c4fe53695f2ee76d54d8452b726da46c
Charlie Grimshaw <noobsicle26@gmail.com>
2017-07-08 21:14:39 +0100

    Made music play in draw() not setup()

--- sketch.js
+++ sketch.js
@@ -6,12 +6,6 @@ function preload () {
 
 function setup() {
 
-  if (song.isPlaying())
-    song.stop();
-  } else {
-    song.play();
-  }
-
   //Creates a canvas the size of window, the -4 is to stop scroller from appearing
   createCanvas(windowWidth - 4, windowHeight - 4);
   //Creates variables for the objects, left and right are the paddles.
@@ -23,29 +17,36 @@ function setup() {
 }
 
 function draw() {
-    //black background
-    background(0);
+  
+  if (song.isPlaying())
+    song.stop();
+  } else {
+    song.play();
+  }
 
-    //every frame the puck checks if it is touching a paddle, if it is, it will bounce off. TODO: make this one method
-    puck.checkPaddleLeft(left);
-    puck.checkPaddleRight(right);
+  //black background
+  background(0);
 
-    //shows and changes position of the paddles
-    left.show();
-    right.show();
-    left.update();
-    right.update();
+  //every frame the puck checks if it is touching a paddle, if it is, it will bounce off. TODO: make this one method
+  puck.checkPaddleLeft(left);
+  puck.checkPaddleRight(right);
 
-    //shows and changes position of the puck, every frame it checks if it is touching top and bottom edge of screens
-    puck.update();
-    puck.edges();
-    puck.show();
+  //shows and changes position of the paddles
+  left.show();
+  right.show();
+  left.update();
+  right.update();
 
-    //white text, size 32, shows score for each paddle TODO: make this look waaayyy nicer
-    fill(255);
-    textSize(32);
-    text(puck.leftscore, 10, 40);
-    text(puck.rightscore, width-20, 40);
+  //shows and changes position of the puck, every frame it checks if it is touching top and bottom edge of screens
+  puck.update();
+  puck.edges();
+  puck.show();
+
+  //white text, size 32, shows score for each paddle TODO: make this look waaayyy nicer
+  fill(255);
+  textSize(32);
+  text(puck.leftscore, 10, 40);
+  text(puck.rightscore, width-20, 40);
 
   //checks for input then moves the paddle accordingly. a/z move left paddle, j/m move right paddle.
   if (keyIsDown(65)) {
